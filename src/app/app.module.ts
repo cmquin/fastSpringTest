c1eff152ef4d63c365baa542bea76422ff55a6c8
cmquin <cmbquin@gmail.com>
2019-01-14 00:48:55 -0800

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UserFormComponent } from './components/user-form/user-form.component';
+import { AdminListComponent } from './components/admin-list/admin-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UserFormComponent,
+    AdminListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/components/user-form/user-form.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms'
+
+@Component({
+  selector: 'app-user-form',
+  templateUrl: './user-form.component.html',
+  styleUrls: ['./user-form.component.css']
+})
+export class UserFormComponent implements OnInit {
+
+  constructor(private form: FormBuilder) { }
+
+  ngOnInit() {
+  }
+
+  create
+
+}
